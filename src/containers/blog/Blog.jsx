1a79fc62f7765a461f644129d74fe3538f7c8a63
kelvinlii <66974765+kelvinlii@users.noreply.github.com>
2022-12-26 06:40:13 -0800

    Add files via upload

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//import Article from '../../components/article/Article';
+
+
+import './blog.css';
+
+const Blog = () => (
+  <div className="gpt3__blog section__padding" id="blog">
+    <div className="gpt3__blog-heading">
+      <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+    </div>
+    
+    </div>
+  
+);
+
+export default Blog;
